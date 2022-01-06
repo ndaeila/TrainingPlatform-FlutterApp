38416c2541e12f296894c232d427f2e2e1f614fd
GithubN0ob <simolanian@gmail.com>
2022-01-05 23:18:09 -0800

    Updated README.md

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 # CaliforniaEfficiencyGroupAPP
 
-Flutter app for California Efficiency Group.
-
-## Getting Started
-
 This project is a training web app for California Efficiency Group.
 
-A few resources to get you started if this is your first Flutter project:
+# Setup
+## Flutter
+**SDK-version** `v2.8.1`
+MacOS: https://flutter.dev/docs/get-started/install/macos
+Windows: https://flutter.dev/docs/get-started/install/windows
+
+## Dart
+**version** `2.15.1`
+
+[Upgrading flutter](https://flutter.dev/docs/development/tools/sdk/upgrading)
+
+## Flutter Library Install
+* Use `flutter pub upgrade` basically
+  * `flutter pub get` can't get newest library because of pubspec.lock
+
+## Recommended VSCode extensions:
+- Flutter
+- Dart
+- Bracket Pair Colorizer
+
+## Getting Started
 
 - [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
@@ -14,3 +30,8 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Helpful to know
+Typically, if there is a build error of some kind, run `flutter clean` then `flutter pub get`.
+
+To generate the chopper and freeze files, run `flutter pub run build_runner build --delete-conflicting-outputs` in the terminal.
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 // Import other stuff
 import 'dart:math';
 
-// Import widgets
 import 'package:californiaefficiencygroup/ui/app.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'ui/widgets/nav-drawer.dart';
-import 'ui/widgets/get-text.dart';
+import 'commons/widgets/nav-drawer.dart';
+import 'commons/widgets/get-text.dart';
 
 // Terminal Comnmands:
 //  check available devices: flutter devices
@@ -34,8 +33,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'California Efficiency Group',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
         primarySwatch: generateMaterialColor(Colors.white),
       ),
       home: const MyHomePage(
@@ -48,15 +45,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -67,19 +55,12 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
     const bodyTextElement =
         ReadTextFile(textURL: './assets/text/DummyText.txt');
     const titleMargin = 10;
@@ -112,23 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
         preferredSize: const Size.fromHeight(kToolbarHeight),
       ),
       body: SingleChildScrollView(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
 
           children: const [
@@ -136,23 +101,12 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
 class MyLoginPage extends StatefulWidget {
   const MyLoginPage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
