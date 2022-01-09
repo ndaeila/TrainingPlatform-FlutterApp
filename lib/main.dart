f0ce5bcf6c129ff8564e418a763d80cd8c254095
na999999 <natedaeila@gmail.com>
2022-01-09 10:31:17 -0800

    firebase changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 // Import other stuff
 import 'dart:math';
 
+// Import firebase
+import 'package:firebase_core/firebase_core.dart';
+
+// Import necessary material
 import 'package:californiaefficiencygroup/ui/app.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +18,7 @@ import 'commons/widgets/get-text.dart';
 //   ios: flutter run
 //   verbose: -v
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MultiProvider(
@@ -25,7 +29,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
@@ -35,8 +40,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: generateMaterialColor(Colors.white),
       ),
-      home: const MyHomePage(
-        title: 'California Efficiency Group',
+      home: FutureBuilder(
+        future: _fbApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("Error: " + snapshot.error.toString());
+            return const Text("Something went wrong!");
+          } else if (snapshot.hasData) {
+            const MyHomePage(title: 'California Efficiency Group');
+          } else {
+            // Show Splash Screen
+          }
+        },
       ),
     );
   }
@@ -52,13 +67,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     const bodyTextElement =
@@ -95,7 +103,6 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-
           children: const [
             bodyTextElement,
           ],
@@ -114,27 +121,8 @@ class MyLoginPage extends StatefulWidget {
 }
 
 class _MyLoginPageState extends State<MyLoginPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       drawer: NavDrawer(),
       appBar: AppBar(
@@ -146,29 +134,11 @@ class _MyLoginPageState extends State<MyLoginPage> {
         ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-
           // body children go here
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
